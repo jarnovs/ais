9752deba6100459e4b0145cb407fff3799bb2fb1
jarnovs <usalmazovich@gmail.com>
2025-08-25 12:48:54 +0600

    fix: auth

--- backend/app/users/utils.py
+++ backend/app/users/utils.py
@@ -5,6 +5,6 @@ def hash_password(password: str) -> str:
     return hashpw(password.encode(), gensalt()).decode()
 
 
-async def check_password(password: str, hashed_password: str) -> bool:
+def check_password(password: str, hashed_password: str) -> bool:
     return checkpw(password.encode(), hashed_password.encode())
 
