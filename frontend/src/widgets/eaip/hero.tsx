afcd24105d3cbc424ca297d5f7678d2d8c51a270
jarnovs <usalmazovich@gmail.com>
2025-08-25 08:56:27 +0600

    fix: hero

--- frontend/src/widgets/eaip/hero.tsx
+++ frontend/src/widgets/eaip/hero.tsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import Image from 'next/image'
+import { Fullscreen } from 'lucide-react'
 
 const Hero = () => {
   return (
     <>
-    <Image width={1920} height={90} src="/Background.png" alt="Background" />
-    <div className="flex flex-col gap-4 max-w-7xl p-4 md:p-10">
+    <Image width={1920} height={90} src="/Background.png" alt="Background" style={{ width: "100%" }} />
+    <div className="flex flex-col gap-4 p-4 md:p-10">
         <div className="flex flex-col items-center">
             <h1 className="mb-4 text-xs text-center md:text-3xl">Aeronautical Information Publication (E-AIP)</h1>
             <Image width={131} height={77} src="/unnamed.png" alt="Logo" />
