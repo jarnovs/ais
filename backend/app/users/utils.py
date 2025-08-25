9a11efece0876980788e3cd26db0abdd641f559b
jarnovs <usalmazovich@gmail.com>
2025-08-25 13:14:38 +0600

    fix: hash

--- backend/app/users/utils.py
+++ backend/app/users/utils.py
@@ -4,5 +4,5 @@ from bcrypt import gensalt, hashpw, checkpw
 def hash_password(password):
     return hashpw(password.encode("utf-8"), gensalt()).decode()
 
-def check_password(password, hashed_password) -> bool:
-    return checkpw(password.encode("utf-8"), hashed_password)
+def check_password(password: str, hashed_password: str) -> bool:
+    return checkpw(password.encode("utf-8"), hashed_password.encode("utf-8"))
