5c22029c9e7053e7f130184c262a3d98abf4b508
Sanjar <usalmazovich@gmail.com>
2025-10-03 02:50:06 +0600

    fix: size mobile

--- frontend/src/widgets/eaip/hero.tsx
+++ frontend/src/widgets/eaip/hero.tsx
@@ -8,10 +8,10 @@ const Hero = () => {
     <Image width={1920} height={90} src="/Background.png" alt="Background" style={{ width: "100%" }} />
     <div className="flex flex-col gap-4 p-4 md:p-10">
         <div className="flex flex-col items-center">
-            <h1 className="mb-4 text-xs text-center md:text-3xl">Aeronautical Information Publication (E-AIP)</h1>
-            <Image width={131} height={77} src="/unnamed.png" alt="Logo" />
-            <h2 className="text-xs md:text-xl">Kyrgyz Republic</h2>
-            <h2 className="text-xs md:text-xl">Consult NOTAM for Latest Information</h2>
+            <h1 className="mb-4 text-xs text-center md:text-2xl font-semibold">Electronic Aeronautical Information Publication (eAIP)</h1>
+            <Image width={131} height={77} src="/unnamed.png" alt="Logo" className="mb-2" />
+            <h1 className="text-xs md:text-xl">Kyrgyz Republic</h1>
+            <h1 className="text-xs md:text-xl">Consult NOTAM for Latest Information</h1>
         </div>
     </div>
     </>
