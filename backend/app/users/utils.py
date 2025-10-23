301250a69bedd7a6de67ab17432a30db515cb5c4
jarnov <jarnov@awork.kg>
2025-10-23 13:34:23 +0600

    fix: update user pass / hash bug

--- backend/app/users/utils.py
+++ backend/app/users/utils.py
@@ -4,5 +4,6 @@ from bcrypt import gensalt, hashpw, checkpw
 def hash_password(password):
     return hashpw(password.encode("utf-8"), gensalt()).decode()
 
+
 def check_password(password: str, hashed_password: str) -> bool:
     return checkpw(password.encode("utf-8"), hashed_password.encode("utf-8"))
